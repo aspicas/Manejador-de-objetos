e8814358323e544ebcd2ad29f989e2b2c41ade8d
David Garcia <david.a.g.r93@gmail.com>
2017-07-10 21:43:31 -0400

    comito

---
+++ ServidorAplicacion/ServidorAplicacion/src/java/resources/MobResource.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package resources;
+
+import java.util.Date;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
+import model.Mob;
+
+/**
+ * REST Web Service
+ *
+ * @author david
+ */
+@Path("/objects")
+@Produces(MediaType.APPLICATION_XML)
+public class MobResource {
+    
+    @GET
+    @Path("/{id}")
+    public Mob getMob(@PathParam("id") int id) {
+        Date date = new Date();
+        return new Mob(id, date, "nombre", "accion");
+    }
+    
+    @POST
+    @Consumes(MediaType.APPLICATION_XML)
+    public Mob addMob(Mob mob) {        
+        return mob;
+    }
+    
+    @DELETE
+    @Path("/{id}")
+    public void deleteMob(@PathParam("id") int id){
+        
+    }
+}
