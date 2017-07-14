a82e297190d494361e9f80823ea6855332b22837
David Garcia <david.a.g.r93@gmail.com>
2017-07-14 01:28:17 -0400

    comito

---
+++ ServidorReplica/src/servidorreplica/ServidorReplicaInterface.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servidorreplica;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ *
+ * @author david
+ */
+public interface ServidorReplicaInterface extends Remote {
+    String replicar(String accion) throws RemoteException;
+    String restaurar(String DocumentoXML) throws RemoteException;
+}
